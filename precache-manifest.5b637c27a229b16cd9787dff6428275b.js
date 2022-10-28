b6f9fef15717695d49e451a706656fc881699d4a
SabbirAhmedChowdhury <sabbir014@gmail.com>
2022-10-28 23:11:46 +0600

    Updates

---
+++ precache-manifest.5b637c27a229b16cd9787dff6428275b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "657408b74bac4c15854e",
+    "url": "/robofriends/static/css/main.4a2ccede.chunk.css"
+  },
+  {
+    "revision": "657408b74bac4c15854e",
+    "url": "/robofriends/static/js/main.657408b7.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "5c20a48b2569e265dd3a",
+    "url": "/robofriends/static/css/2.59450941.chunk.css"
+  },
+  {
+    "revision": "5c20a48b2569e265dd3a",
+    "url": "/robofriends/static/js/2.5c20a48b.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "af3608e6d1ab639d7a94815719eb39d6",
+    "url": "/robofriends/index.html"
+  }
+];
